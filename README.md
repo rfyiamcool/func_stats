87593a1dd753d0a2d00b04b6e6dfd1abc6162f4e
rfyiamcool <rfyiamcool@163.com>
2016-06-27 22:50:02 +0800

    for test

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # func_stats
-# func_stats
+
+使用员表格方式显示函数调用统计
---
+++ func_stats/func_stats.py
@@ -0,0 +1,70 @@
+from __future__ import print_function
+
+from collections import OrderedDict
+from time import time
+from .prettytable import PrettyTable
+
+import inspect
+
+class CreateStats(OrderedDict):
+
+    def __init__(self):
+        super(CreateStats, self).__init__()
+        self['_start'] = time()
+        self.last_mark = self['_start']
+        self.auto_increment = 1
+        self.marker_code = {}
+
+    def mark(self, marker_name=None, current_frame=None):
+
+        if marker_name is None:
+            marker_name = self.auto_increment
+            self.auto_increment += 1
+
+        if not self.get(marker_name):
+            self[marker_name] = []
+
+        time_elapsed = time() - self.last_mark
+        self.last_mark = time()
+
+        self[marker_name].append(time_elapsed)
+        if current_frame is None:
+            current_frame = inspect.currentframe()
+        last_frame = current_frame.f_back
+        self.marker_code[marker_name] = (
+            last_frame.f_code.co_filename,
+            last_frame.f_lineno,
+            last_frame.f_code.co_name
+        )
+
+
+    def stop(self):
+        self['_end'] = time()
+
+    def display(self):
+        x = PrettyTable(["Marker", "Method", "Line", "Hits", "Avg Time", "Runtime", "Percent"])
+        x.align["Marker"] = "l"
+        x.align["Hits"] = "r"
+        x.align["Avg Time"] = "r"
+        x.align["Runtime"] = "r"
+        x.align["Percent"] = "r"
+        x.align["Method"] = "l"
+        x.align["Line"] = "r"
+        x.padding_width = 1
+        if '_end' not in self:
+            self['_end'] = time()
+        total_time = self['_end'] - self['_start']
+        for marker, runtimes in self.items():
+            if type(runtimes) is not list:
+                continue
+            x.add_row((
+                marker,
+                self.marker_code[marker][2],
+                self.marker_code[marker][1],
+                len(runtimes),
+                "{0:.5f}".format(sum(runtimes) / float(len(runtimes))),
+                "{0:.5f}".format(sum(runtimes)),
+                "{0:.2f}".format(sum(runtimes) / total_time * 100),
+            ))
+        print(x)
+        print("Total runtime: {0:.2f}".format(total_time))
